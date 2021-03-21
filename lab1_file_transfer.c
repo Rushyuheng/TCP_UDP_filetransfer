b0fc227214e7945994ee76ad684c26fb9cc3dbd5
james_rush <jamesyhh@gmail.com>
2021-03-21 21:41:27 +0800

    tcp no complie error

--- lab1_file_transfer.c
+++ lab1_file_transfer.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 //unix library
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
@@ -16,6 +18,13 @@ void errormsg(const char * msg){
     perror(msg);
     perror("program stop");
     exit(EXIT_FAILURE);
+}
+
+void showprogress(int percent){
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    printf("%d %%",percent);
+    printf(" %s\n", asctime(tm));
 }
 
 int main(int argc, char const *argv[])
@@ -30,7 +39,7 @@ int main(int argc, char const *argv[])
 
         socketd = socket(AF_INET,SOCK_STREAM,0); // construct socket in IPv4 form with TCP protocol
 
-        bzero((char *) &serv_addr, sizeof(serv_addr)); //clear byte to 0 for intializing serv_addr struct
+        memset(&serv_addr, 0, sizeof(serv_addr));//clear every byte in struct for intializing serv_addr struct
 
         portnum = atoi(argv[4]); 
         serv_addr.sin_family = AF_INET; // config IP form as IPv4
@@ -54,18 +63,28 @@ int main(int argc, char const *argv[])
             }
 
             //----The code we should revise----
-            bzero(buffer,256); // clear buffer
-            rw_status = read(newsocketd,buffer,255);
-            if (rw_status < 0)
-                errormsg("fail to read from socket");
-            printf("Here is the message: %s\n",buffer);
+            FILE *fp = fopen("recv.txt", "w");
+            if(fp == NULL){
+                errormsg("fail to open write file");
+            }
+            else{
+                bzero(buffer,256); // clear buffer perpare to recv data from socket
+                int length = 0;
+                while(length = recv(newsocketd, buffer, 256, 0)){ // recv data from socket
+                        if (length < 0)
+                            errormsg("fail to recv data from socket");
 
-            rw_status = write(newsocketd,"I got your message",18);
-            if (rw_status < 0)
-                errormsg("fail to write to socket");
-            //--------
+                        int write_length = fwrite(buffer, sizeof(char), length, fp);//write buffer to file
+                        if(write_length < length)
+                            errormsg("fail to write data from recv buffer to file");
 
-            //close socket and end file transfering
+                        bzero(buffer, 256);//clear buffer for next loop
+                } 
+            }
+            printf("file receive complete");
+
+            // close socket and file pointer
+            fclose(fp);
             close(newsocketd);
             close(socketd);
         }
@@ -75,21 +94,43 @@ int main(int argc, char const *argv[])
                 errormsg("fail to connect socket");
             }
 
-            //----The code we should revise----
-            printf("Please enter the message:\n");
-            bzero(buffer,256);
-            fgets(buffer,255,stdin);
+            FILE *fp = fopen(argv[5],"r"); //open file and creat file pointer
+            if(fp == NULL){
+                errormsg("fail to open file");
+            }
+            else{
+                bzero(buffer,256); // clear buffer
+                int file_block_length = 0;
+                int sended_block_length = 0;
 
-            rw_status = write(socketd,buffer,strlen(buffer));
-            if (rw_status < 0) 
-                errormsg("fail to write to socket");
-            bzero(buffer,256);
+                struct stat st; //get file size from system
+                stat(argv[5], &st);
+                float filesize = st.st_size;
+                float filesizeMB = filesize / 1024.0 / 1024.0; // byte to MB
 
-            rw_status = read(socketd,buffer,255);
-            if (rw_status < 0) 
-                errormsg("fail to read from socket");
-            printf("%s\n",buffer);
-            //--------
+                int progressindex = 1;
+                clock_t t1, t2;
+
+                showprogress(0);//print time and progress
+
+                t1 = clock();
+                while( (file_block_length = fread(buffer, sizeof(char), 256, fp)) > 0){//read file into buffer
+                    if (send(socketd, buffer, file_block_length, 0) < 0)// send file to socket
+                        errormsg("fail to send file");
+                    
+                    sended_block_length += file_block_length;
+                    if(sended_block_length / filesize >= progressindex * 0.25){ //print progress evert 25%
+                        showprogress((int) progressindex * 0.25);
+                        ++progressindex;
+                    }
+                    bzero(buffer, sizeof(buffer));//clear buffer for next loop
+                }
+                fclose(fp);
+                t2 = clock();
+                showprogress(100);
+                double elapsed = ((double)t2 - t1) / CLOCKS_PER_SEC * 1000;
+                printf(" total transfer time : %f ms",elapsed );
+            }
 
             close(socketd);//close socket and end file transfering
         }
